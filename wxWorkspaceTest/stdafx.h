bd17197487278d3df82abf52820377c287c3cba6
glow <glow@stuifmeel.org>
2007-12-03 20:25:41 +0000

    Cleaned up the project file configurations. It works fine now as long as your wxWidgets dir is placed at ../wxWidgets-2.8.0 from our solution file.
    PS. If this doesn't link okay, there's a large change you'll have to change the Code Generation type of both projects to "Multithreaded (Debug|Release) DLL", instead of the "Multithreaded (Debug|Release)" I am using currently.

--- wxWorkspaceTest/stdafx.h
+++ wxWorkspaceTest/stdafx.h
@@ -1,22 +1,5 @@
 #pragma once
 
 #include <wx/wx.h>
-#include "../wxWorkspaceView/wxWorkspaceView.h"
+#include <wxWorkspaceView.h>
 
-#pragma comment(lib, "../debug/wxWorkspaceViewd.lib")
-#pragma comment(lib, "wxmsw28d_adv.lib")
-#pragma comment(lib, "wxmsw28d_html.lib")
-#pragma comment(lib, "wxbase28d_xml.lib")
-#pragma comment(lib, "wxmsw28d_core.lib")
-#pragma comment(lib, "wxbase28d.lib")
-#pragma comment(lib, "wxtiffd.lib")
-#pragma comment(lib, "wxjpegd.lib")
-#pragma comment(lib, "wxpngd.lib")
-#pragma comment(lib, "wxzlibd.lib")
-#pragma comment(lib, "wxregexd.lib")
-#pragma comment(lib, "wxexpatd.lib")
-#pragma comment(lib, "winmm.lib")
-#pragma comment(lib, "comctl32.lib")
-#pragma comment(lib, "rpcrt4.lib")
-#pragma comment(lib, "wsock32.lib")
-#pragma comment(lib, "odbc32.lib")
